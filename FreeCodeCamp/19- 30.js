2b98a013bab572b4dedbc1b64d17e4e95b8934f6
Samir Karimov <80761616+KarimovSamir@users.noreply.github.com>
2025-08-30 23:43:43 +0400

    Update 19- 30.js

--- FreeCodeCamp/19- 30.js
+++ FreeCodeCamp/19- 30.js
@@ -87,3 +87,21 @@ console.log(cities);
 
 // Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
 
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+
+// Step 27
+
+// Another method essential for this project is the .pop() method. It removes the last element from an array and returns that element.
+
+// When a method returns a value, you can think of it as giving the value back to you, making it available for use in other parts of your code.
+
+// Create a new variable called popped and assign it the result of rows.pop(). Then, log popped to the console.
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped;
+popped = rows.pop();
+console.log(popped);
+
