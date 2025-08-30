4b25b9e2e324555abe7282b7de441ddd8faeb263
Samir Karimov <80761616+KarimovSamir@users.noreply.github.com>
2025-08-30 22:30:03 +0400

    Create 13-18.js

---
+++ FreeCodeCamp/13-18.js
@@ -0,0 +1,52 @@
+// Step 13
+
+// Before moving forward, you should take a moment to review the concepts you have learned.
+
+// Use the let keyword to declare a profession variable and an age variable. Initialize profession with the string "teacher", but do not initialize age with any value.
+
+// Log both of your variables to the console to see the results.
+
+let profession;
+let age;
+profession = "teacher";
+console.log(profession);
+console.log(age);
+
+// Step 15
+
+// Use let to declare a count variable. Assign it the number 8. When using a number value, you do not use quotes.
+
+let character = 'Hello';
+let count = 8;
+
+// Step 16
+
+// With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
+
+let character1 = 'Hello';
+let count1 = 8;
+console.log(count + 1);
+
+// Step 17
+
+// You can also perform subtraction (-), multiplication (*), and division (/). Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.
+
+let count2 = 8;
+
+// Step 18
+
+// In programming, you will often need to work with lots of data. There are many data structures that can help you organize and manage your data. One of the most basic data structures is an array.
+
+// An array is a non-primitive data type that can hold a series of values. Non-primitive data types differ from primitive data types in that they can hold more complex data. Primitive data types like strings and numbers can only hold one value at a time.
+
+// Arrays are denoted using square brackets ([]). Here is an example of a variable with the value of an empty array:
+// Example Code
+
+// let array = [];
+
+// Declare a rows variable and assign it an empty array.
+
+let character3 = 'Hello';
+let count3 = 8;
+let rows = [];
+
