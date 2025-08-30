ac516d20b467dcf196ee4e4be96f743868d0b6d0
Samir Karimov <80761616+KarimovSamir@users.noreply.github.com>
2025-08-30 22:14:28 +0400

    Update 9-12.js

--- FreeCodeCamp/9-12.js
+++ FreeCodeCamp/9-12.js
@@ -27,3 +27,29 @@ character = "World";
 let secondCharacter1;
 secondCharacter = "Test";
 console.log(secondCharacter);
+
+// Step 11
+
+// You can also assign the value of a variable to another variable. For example:
+// Example Code
+
+// let first = "One";
+// let second = "Two";
+// second = first;
+
+// The second variable would now have the value "One".
+
+// To see this in action, change your secondCharacter assignment from "Test" to your character variable.
+
+// Then open the console to see what gets logged.
+
+secondCharacter = character;
+console.log(secondCharacter);
+
+// Step 12
+
+// You are now ready to declare your next variable. Remove both console.log statements, and the character reassignment.
+
+// Also remove your secondCharacter variable, but leave the character initialization unchanged.
+
+let character2 = 'Hello';
