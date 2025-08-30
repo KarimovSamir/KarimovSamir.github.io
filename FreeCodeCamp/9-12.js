b01fea18b6aff54f5a9d5490206ba1c96364c042
Samir Karimov <80761616+KarimovSamir@users.noreply.github.com>
2025-08-30 22:00:03 +0400

    Create 9-12.js

---
+++ FreeCodeCamp/9-12.js
@@ -0,0 +1,29 @@
+// Step 9
+
+// When you declare a variable without initializing it, it is considered uninitialized. Currently, your secondCharacter variable is uninitialized.
+
+// Add a console.log() to see what the value of your secondCharacter variable is.
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
+
+// Step 10
+
+// The default value of an uninitialized variable is undefined. This is a special data type that represents a value that does not have a definition yet.
+
+// You can still assign a value to an uninitialized variable. Here is an example:
+// Example Code
+
+// let uninitialized;
+// uninitialized = "assigned";
+
+// Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+
+let character1 = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter1;
+secondCharacter = "Test";
+console.log(secondCharacter);
